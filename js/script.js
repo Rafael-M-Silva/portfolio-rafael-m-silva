1c657f6bd31635657f38c256b330d187685f3d4d
Rafael Mauricio <rafael_mauricio@hotmail.com>
2024-02-14 19:14:57 -0300

    style: responsivo 80%

--- js/script.js
+++ js/script.js
@@ -1,12 +1,9 @@
 const dropDown = (event) => {
   const btn = event.target;
-  const faq = btn.closest('.faq'); // Encontrar o elemento .faq pai do botão clicado
-  const description = faq.querySelector('p'); // Selecionar o <p> dentro do .faq
-  description.classList.toggle('slide-down');
-  btn.classList.toggle('btn-rotate')
-}
+  btn.classList.toggle("btn-rotate");
+};
 
-const allBtn = document.querySelectorAll('.btn-slide-down')
-allBtn.forEach(btn => {
-  btn.addEventListener('click', dropDown)
+const allBtn = document.querySelectorAll(".btn-slide-down");
+allBtn.forEach((btn) => {
+  btn.addEventListener("click", dropDown);
 });
